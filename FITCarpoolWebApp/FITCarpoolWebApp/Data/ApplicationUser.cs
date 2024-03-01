9f42642d353adf152af41cb918fd29a94071ba38
Austin Phillips <austinphillipssps@gmail.com>
2024-03-01 10:49:34 -0500

    Commit

--- FITCarpoolWebApp/FITCarpoolWebApp/Data/ApplicationUser.cs
+++ FITCarpoolWebApp/FITCarpoolWebApp/Data/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace FITCarpoolWebApp.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Role { get; set; } = "Rider";
+       
+
     }
 
 }
---
+++ FITCarpoolWebApp/FITCarpoolWebApp/Data/Migrations/20240301150913_RoleAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FITCarpoolWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "Rider");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "AspNetUsers");
+        }
+    }
+}
