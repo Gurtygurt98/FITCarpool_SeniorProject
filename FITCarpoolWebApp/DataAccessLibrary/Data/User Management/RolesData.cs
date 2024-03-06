a1bb0947693464f77517867bd15911d0cda24670
Austin Phillips <austinphillipssps@gmail.com>
2024-03-06 09:54:16 -0500

    Test Push

---
+++ FITCarpoolWebApp/DataAccessLibrary/Data/User Management/RolesData.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Data.User_Management
+{
+    public class RolesData
+    {
+        private readonly ISQLDataAccess _db;
+
+        public RolesData(ISQLDataAccess db)
+        {
+            _db = db;
+        }
+        public Task<List<RolesModel>> GetUserRoles()
+        {
+            string sql = " ";
+            return _db.LoadData<RolesModel, dynamic>(sql, new { });
+        }
+    }
+}
