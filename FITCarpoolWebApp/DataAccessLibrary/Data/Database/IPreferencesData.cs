531901c528c6bf5b5ec6b1f61ed7c036c3d48b90
Austin Phillips <a.phillips@winconn.com>
2024-03-14 18:12:09 -0400

    Add Prefernces DB context

--- FITCarpoolWebApp/DataAccessLibrary/Data/Database/IPreferencesData.cs
+++ FITCarpoolWebApp/DataAccessLibrary/Data/Database/IPreferencesData.cs
@@ -4,9 +4,10 @@ namespace DataAccessLibrary.Data.Database
 {
     public interface IPreferencesData
     {
+        Task AddUser(int userId);
         Task DeletePreference(int userId);
         Task<List<PreferencesModel>> GetAllPreferences();
-        Task<PreferencesModel> GetPreference(int userId);
+        Task<List<PreferencesModel>> GetPreference(int userId);
         Task UpdatePreference(PreferencesModel preference);
     }
 }
--- FITCarpoolWebApp/DataAccessLibrary/Model/Database Models/PreferencesModel.cs
+++ FITCarpoolWebApp/DataAccessLibrary/Model/Database Models/PreferencesModel.cs
@@ -9,15 +9,26 @@ namespace DataAccessLibrary.Model
     public class PreferencesModel
     {
         public int UserId { get; set; }
-        public string PreferenceValue { get; set; }
+        public string GenderPreference { get; set; } = "No Preference";
+        public string EatingPreference { get; set; } = "No Preference";
+        public string SmokingPreference { get; set; } = "No Preference";
+        public string TemperaturePreference { get; set; } = "No Preference";
+        public string MusicPreference { get; set; } = "No Preference";
 
         public PreferencesModel() { }
 
-        public PreferencesModel(int userId, string preferenceValue)
+        public PreferencesModel(int userId, string genderPreference, string eatingPreference, string smokingPreference, string temperaturePreference , string musicPreference)
         {
             UserId = userId;
-            PreferenceValue = preferenceValue;
+            GenderPreference = genderPreference;
+            EatingPreference = eatingPreference;
+            SmokingPreference = smokingPreference;
+            TemperaturePreference = temperaturePreference;
+            MusicPreference = musicPreference;
+        }
+        public PreferencesModel(int userId)
+        {
+            UserId = userId;
         }
     }
-
 }
