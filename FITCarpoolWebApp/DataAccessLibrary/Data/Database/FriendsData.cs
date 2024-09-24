5434ade9a1cbe1408dddd07c17d220f30a0bc3c1
Hunter Smith <hsmith2021@my.fit.edu>
2024-09-24 09:40:58 -0400

    Started database integration with Friends List

--- FITCarpoolWebApp/DataAccessLibrary/Data/Database/FriendsData.cs
+++ FITCarpoolWebApp/DataAccessLibrary/Data/Database/FriendsData.cs
@@ -19,6 +19,11 @@ namespace DataAccessLibrary.Data.Database
             string sql = "SELECT * FROM Friends";
             return await _db.LoadData<FriendsModel, dynamic>(sql, new { });
         }
+        public async Task<List<FriendsModel>> GetFriends(int userId)
+        {
+            string sql = "SELECT * FROM Friends WHERE UserID1 = @UserId OR UserID2 = @UserId";
+            return await _db.LoadData<FriendsModel, dynamic>(sql, new { UserId = userId });
+        }
 
         public async Task<FriendsModel> GetFriend(int friendshipId)
         {
@@ -30,6 +35,12 @@ namespace DataAccessLibrary.Data.Database
         public async Task UpdateFriend(FriendsModel friend)
         {
             string sql = @"UPDATE Friends SET UserID1 = @UserId1, UserID2 = @UserId2, Status = @Status, CreatedDate = @CreatedDate WHERE FriendshipID = @FriendshipId";
+            await _db.SaveData(sql, friend);
+        }
+
+        public async Task AddFriend(FriendsModel friend)
+        {
+            string sql = @"INSERT INTO Friends (UserID1, UserID2, Status, CreatedDate, FriendshipID) VALUES (@UserId1, @UserId2, @Status, @CreatedDate, @FriendshipId)";
             await _db.SaveData(sql, friend);
         }
 
